c25291ef4a311fa1a83b74ccc299f4feedf46cd1
Samuel CADART <samuel.cadart@malteurop.com>
2024-04-22 14:37:43 +0200

    Ajout de snyk a GitHub Action

---
+++ .github/workflows/snyk.py
@@ -0,0 +1,91 @@
+import json
+from colorama import init, Fore
+
+def fromJSON(data:str):
+    with open(data,'r') as file:
+        res = file.read()
+        json_data = json.loads(res)
+        return json_data
+
+class Vulnerabilite(object):
+    def __init__(self,nom:str,sev:str,ind:int,info:str,ligne:str) -> None:
+        self.nom = nom 
+        self.sev = sev
+        self.ind = ind
+        self.info = info
+        self.ligne = ligne
+    def __repr__(self) -> str:
+        return f"[{self.sev}] {self.nom}\n\tPath: {self.ligne}\n\tInfo: {self.info}"
+
+    def __str__(self) -> str:
+        return f"<p {self.color()}>[{self.sev}] {self.nom}</p><ul><li>PATH: {self.ligne}</li><li>INFO: {self.info}</li></ul>"
+    def color(self):
+        if self.sev == "Low":
+            return "style='color:#5cd65c'"
+        elif self.sev == "Medium":
+            return "style='color:#ff8000'"
+        elif self.sev == "High":
+            return "style='color:#ff3300'"
+        else:
+            return "style='color:#cc3399'"
+
+class SnykAnalyser(object):
+    def __init__(self,file:str="snyk.json") -> None:
+        self.jsonFile = fromJSON(file)
+        self.ruleIndex = []
+        self.score = [400,700,900,1001]
+        self.etiquette = ["Low","Medium","High","Critical"]
+        self.nbVuln = 0
+        self.vuln:Vulnerabilite = []
+
+    def __str__(self) -> str:
+        for i in self.vuln:
+            print(str(i))
+        return f"\nSummary:\n\n\t{self.nbVuln} Code issues found"
+
+    def __call__(self):
+        snyk = self.jsonFile["runs"][0]
+        rules = snyk["tool"]["driver"]["rules"]
+        init()
+
+        for i in rules:
+            self.ruleIndex.append(i["shortDescription"]["text"])
+        
+        vuln = snyk["results"]
+
+        for i in vuln:
+            self.nbVuln += 1
+            name = self.ruleIndex[i['ruleIndex']]
+            score = int(float(i['properties']['priorityScore']))
+            severity = ""
+            for j in range(len(self.score)-1,-1,-1):
+                if score>=self.score[j]:
+                    severity = self.etiquette[j+1]
+                    break
+            if(severity == ""):
+                severity = self.etiquette[0]
+            info = i['message']['text']
+            ligne = f"{i['locations'][0]['physicalLocation']['artifactLocation']['uri']}, line {i['locations'][0]['physicalLocation']['region']['startLine']}"
+            tmp = Vulnerabilite(name,severity,score,info,ligne)
+            self.vuln.append(tmp)
+        self.vuln.sort(key=lambda x: x.ind)
+
+myAnalyser = SnykAnalyser()
+
+myAnalyser()
+
+print("""
+<!DOCTYPE html>
+      <html>
+        <head>
+            <title>Code Issues</title>
+        </head>
+        <body>
+""")
+
+print(myAnalyser)
+
+print("""
+      </body>
+      </html>
+""")
